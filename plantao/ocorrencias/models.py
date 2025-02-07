49a4b2dd6ff7c24924850b78f67719ab54447908
Jessé Júnior <jessecarvalho333@gmail.com>
2025-02-07 15:14:49 -0300

    ajuste

--- plantao/ocorrencias/models.py
+++ plantao/ocorrencias/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Unidades(models.Model):
+class Unidade(models.Model):
     nome = models.CharField(max_length=100, unique=True) 
 
     def __str__(self):
@@ -10,12 +10,12 @@ class Unidades(models.Model):
     
 class Bairro(models.Model):
     nome = models.CharField(max_length=100)
-    unidade = models.ForeignKey(Unidade, on_delete=models.CASCADE)
+    unidade = models.ForeignKey(Unidade, on_delete=models.CASCADE, related_name="bairros")
 
     def __str__(self):
         return f"{self.nome} - {self.unidade.nome}"
 
-class Ocorrencia(models.model):
+class Ocorrencia(models.Model):
     codigo = models.IntegerField(unique=True)
 
     def __str__(self):
@@ -25,7 +25,7 @@ class DataSolicitacao(models.Model):
     data = models.DateField()
 
     def __str__(self):
-        return self.data.strftime("%d/%m/%y")
+        return self.data.strftime("%d/%m/%Y")
 
 class Parecer(models.Model):
     descricao = models.CharField(max_length=100)

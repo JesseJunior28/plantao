293a6f841750a4cbed2542527637474ef81b4cad
Jessé Júnior <jessecarvalho333@gmail.com>
2025-02-10 11:37:10 -0300

    Classes importadas no admin

--- plantao/ocorrencia/admin.py
+++ plantao/ocorrencia/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
+# Importar as classes
+
+from .models import Unidade, Bairro, Ocorrencia, DataSolicitacao, Parecer, SituacaoAguaCliente
+
 # Register your models here.
+admin.site.register(Unidade)
+admin.site.register(Bairro)
+admin.site.register(Ocorrencia)
+admin.site.register(DataSolicitacao)
+admin.site.register(Parecer)
+admin.site.register(SituacaoAguaCliente)

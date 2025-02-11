76e0c61afd9df873ce42a31635d3113e9cc8dbd9
Jessé Júnior <jessecarvalho333@gmail.com>
2025-02-11 14:43:31 -0300

    Ajuste de ocorrencia

--- plantao/ocorrencia/models.py
+++ plantao/ocorrencia/models.py
@@ -33,17 +33,10 @@ class SituacaoAguaCliente(models.Model):
     def __str__(self):
         return self.descricao
     
-class Ocorrencia(models.Model):
-    titulo = models.CharField(max_length=200)
-    descricao = models.TextField()
-    unidade = models.ForeignKey(Unidade, on_delete=models.CASCADE)
-    bairro = models.ForeignKey(Bairro, on_delete=models.CASCADE, default=1)
-    data_solicitacao = models.ForeignKey(DataSolicitacao, on_delete=models.CASCADE)
-    parecer = models.ForeignKey(Parecer, on_delete=models.CASCADE)
-    situacao_agua_cliente = models.ForeignKey(SituacaoAguaCliente, on_delete=models.CASCADE)
-    codigo = models.IntegerField(unique=True, null=True, blank=False)  # Permitir nulo, mas não em branco
-
+class Ocorrencia(models.model):
+    codigo = models.IntegerField(unique=True)
+    
     def __str__(self):
-        return self.titulo
+        return str(self.codigo)
     
     

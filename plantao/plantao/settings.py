3cae76b25a97e0673d199d709c7911f20db717f7
Jessé Júnior <jessecarvalho333@gmail.com>
2025-03-17 08:37:09 -0300

    Criação da app usuarios para autenticação

--- plantao/plantao/settings.py
+++ plantao/plantao/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ocorrencia.apps.OcorrenciaConfig',
+    'usuarios.apps.UsuariosConfig',
     'crispy_forms',
-    'crispy_bootstrap5'
-    
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [

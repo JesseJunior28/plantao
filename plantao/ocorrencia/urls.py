164ba6358f887ade450e06119175a54f07f43a82
jesse.junior <jesse.junior@aegea.com.br>
2025-05-08 14:01:15 -0300

    .

--- plantao/ocorrencia/urls.py
+++ plantao/ocorrencia/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from ocorrencia.views import lista_ocorrencia, cadastrar_ocorrencia,\
     editar_ocorrencia, excluir_ocorrencia, adicionar_comentario,\
-    concluir_ocorrencia, iniciar_plantao
+    concluir_ocorrencia, iniciar_plantao, concluir_ocorrencia
 
 urlpatterns = [
     path('', lista_ocorrencia, name='lista_ocorrencia'),
@@ -9,7 +9,6 @@ urlpatterns = [
     path('cadastrar/', cadastrar_ocorrencia, name='cadastrar_ocorrencia'),
     path('editar/<int:ocorrencia_id>/', editar_ocorrencia, name='editar_ocorrencia'),
     path('excluir/<int:ocorrencia_id>/', excluir_ocorrencia, name='excluir_ocorrencia'),
-    # path('comentario/<int:id>/', comentario_ocorrencia, name='comentario_ocorrencia'),
     path('adicionar_comentario/', adicionar_comentario, name='adicionar_comentario'),
     path('concluir/<int:ocorrencia_id>/', concluir_ocorrencia, name='concluir_ocorrencia'),
 ]
